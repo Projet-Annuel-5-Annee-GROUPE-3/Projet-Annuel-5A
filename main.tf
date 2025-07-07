64c1f5860bb0f246551b7cc94efc967842702561
equere78 <115066307+equere78@users.noreply.github.com>
2025-07-07 15:06:06 +0200

    Update main.tf
    Ajout suppression generated_kubeconfig.yaml après un destroy

--- main.tf
+++ main.tf
@@ -2,21 +2,37 @@ provider "aws" {
   region = "eu-west-1"
 }
 
+data "aws_eks_cluster" "eks" {
+  name = aws_eks_cluster.eks.name
+}
 
+data "aws_eks_cluster_auth" "eks" {
+  name       = aws_eks_cluster.eks.name
+  depends_on = [aws_eks_cluster.eks]
+}
 
 provider "kubernetes" {
-  host                   = data.aws_eks_cluster.eks.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.eks.certificate_authority[0].data)
+  host                   = aws_eks_cluster.eks.endpoint
+  cluster_ca_certificate = base64decode(aws_eks_cluster.eks.certificate_authority[0].data)
   token                  = data.aws_eks_cluster_auth.eks.token
 }
 
 resource "local_file" "kubeconfig" {
-  content  = templatefile("${path.module}/kubeconfig.tpl", {
-    endpoint            = data.aws_eks_cluster.eks.endpoint
-    cluster_auth_base64 = data.aws_eks_cluster.eks.certificate_authority[0].data
+  content = templatefile("${path.module}/kubeconfig.tpl", {
+    endpoint            = aws_eks_cluster.eks.endpoint
+    cluster_auth_base64 = aws_eks_cluster.eks.certificate_authority[0].data
     token               = data.aws_eks_cluster_auth.eks.token
   })
   filename = "${path.module}/generated_kubeconfig.yaml"
+
+  depends_on = [
+    aws_eks_cluster.eks,
+    aws_eks_node_group.gadget_nodes
+  ]
+
+  lifecycle {
+    create_before_destroy = true
+  }
 }
 
 provider "helm" {
@@ -25,11 +41,13 @@ provider "helm" {
   }
 }
 
+resource "null_resource" "delete_kubeconfig" {
+  triggers = {
+    always_run = timestamp()
+  }
 
-data "aws_eks_cluster" "eks" {
-  name = "eks-GadgetSphere"
-}
-
-data "aws_eks_cluster_auth" "eks" {
-  name = "eks-GadgetSphere"
+  provisioner "local-exec" {
+    when    = destroy
+    command = "rm -f ${path.module}/generated_kubeconfig.yaml"
+  }
 }
